6779eddb82c8bbc62116b22fe57005ea6262c982
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-07-30 07:43:59 +0300

    readme

--- data_structures_algorth/linked_list.py
+++ data_structures_algorth/linked_list.py
@@ -2,26 +2,67 @@
 
 #node class in order to have values and pointer to next values
 
-class Node:
-    def __init__(self,value, ):
-        self.value = value
-        self.pointer= None
+#from _typeshed import Self
 
+
+class Node:
+    def __init__(self,value='' ):
+        self.value = value
+        self.next= None
+    
     
 
 
 
 
 class Linked_list:
+    
     def __init__(self):
         self.head = None
 
-    
-    #insert method to insert new node with a value to the head of the link list
+    all_values = []
+    #insert method to insert new node with a value to the head of the link list and it take three step:
+    # 1- instantiation of new node with the value neing passed, 2-checking if header is pointing to first value then to make the next value pointer point to the header value which will be our next value after creation on new node 3- make the header pointing to the new node value
     def insert(self,value):
-        self.head.value
+
+        
 
         #instanitation of node to pass the value to it in order to  added to it
         node = Node(value)
+        
+        #to point the next pointer to the next value which is the current value but first need to check if the head is created meaning is pointning to vlue as if not I can't decide whic is the next value yet
+        if self.head:
+            node.next = self.head
+        
+        # we want the head to point to the new node value as the first value 
+        self.head = node.value
 
-        #to point the heade to the new value
+        #updating 
+       # it suppose to return nothing only inserting value
+    
+     # checking if a value does exist inside the 
+    
+    def includes(self,value):
+        if self.head == value:
+           
+            return True
+        else:
+            return False
+
+  
+    
+
+    # good representation of all the values(nodes) inside the linked list
+
+if __name__ == '__main__' :
+   num = Linked_list()
+   num.insert(1)
+   num.insert(2)
+   num.insert(3)
+   print(num.includes(3))
+
+   integ = Linked_list()
+   integ.insert(5)
+   integ.insert(7)
+   
+   print(integ.includes(5)) 
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,18 +1,51 @@
 
 import pytest
 
-from data_structures_algorth.linked_list import Linked_list
+from data_structures_algorth.linked_list import Linked_list,Node
+
+#test creation of node
+def test_create_node():
+       assert Node()
+
+# test Node is contructed with data and it is assigned to the attribute "value"
+def test_node_constructed_with_data():
+       first_node = Node('Hi_1')
+       expected = 'Hi_1'
+       actual = first_node.value
+       assert actual == expected
+
+def test_node_next():
+       second_node = Node('Hi_2')
+       actual = second_node.next   
+       assert True
 
 def test_linked_list_class_instant():
        assert Linked_list()
+
+def test_lined_list_head():
+        other_list = Linked_list()
+        actual = other_list.head
+        assert True
   
 def test_linked_list_insert():
-       test = Linked_list()
+       new_list = Linked_list()
 
        with pytest.raises(AttributeError):
-              test.head.value
+              new_list.head.value
+       expected = 1
+       new_list.insert(1)
+       actual = new_list.head
+       
+       assert actual == expected
 
-       test.insert('Hi')
-       actual = test.head.value
-       expected = 'Hi'
+def test_linked_list_multiple_insert():
+       other_list = Linked_list()
+       with pytest.raises(AttributeError):
+              other_list.head.value
+              #ignore the error of head value of none
+       other_list.insert(1)
+       other_list.insert(2)
+       expected = 2
+       actual = other_list.head
+       
        assert actual == expected
