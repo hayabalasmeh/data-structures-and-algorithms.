a7573071b2ab2d9a72b992911e14390348541144
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-02 08:05:36 +0300

    adding final notes

--- data_structures_algorth/challenge_linked_list_insertion/linked_list_insertion.py
+++ data_structures_algorth/challenge_linked_list_insertion/linked_list_insertion.py
@@ -24,21 +24,68 @@ class LinkedListInsertion:
         if self.head == None:
             
            self.head = node 
-           
+        
            return #in order to stop here in first time 
-
         current = self.head # to tranverse through the list
 
         while current.next: 
-            current = current.next  #to tranverse through the nex unti reach the null
+            current = current.next  #to tranverse through the nex until reach the null
         
         current.next = node # when the current.next equal to none assign the new obj to it
 
-
         
 
+    def add_after(self,value,new_value):
+        node = Node(new_value)
+      
+        if self.head == None:
+            self.head = node
+            
+        
+        current = self.head
 
-    
+        holder = None
+        
+        while current.next: 
+            holder = current
+           
+         #to tranverse through the nex until reach the value
+            if (holder.value == value):
+                node.next = current.next
+                holder.next  =  node
+                # when the current.next equal to none assign the new obj to it
+                return
+            current = current.next     
+        
+    def add_before(self,value,new_value):
+        node = Node(new_value)
+      
+        if self.head == None:
+            self.head = node
+            
+        
+        current = self.head
+
+        holder = None
+        
+        while current.value != value: 
+            holder = current
+           
+         #to tranverse through the nex until reach the value
+            # if (holder.value == value):
+             
+                # when the current.next equal to none assign the new obj to it
+                # return
+            current = current.next 
+        if (current != None):
+           node.next = holder.next
+           holder.next  =  node
+        
+        
+        
+                        
+        value = node
+
     def __iter__(self):
 
         current = self.head
@@ -62,7 +109,8 @@ if __name__ == '__main__' :
         num.append(2)
         num.append(3)
         num.append(5)
-        #node = Node(1)
+        num.add_before(7,6)
+        # num.add_before(3,4)
         print(num)
         print(num)    
 
--- tests/test_linked_list_insertion.py
+++ tests/test_linked_list_insertion.py
@@ -19,6 +19,49 @@ def test_adding_to_end():
     first_obj.append(3)
     first_obj.append(7)
     
+    
     expected = str(first_obj)
     actual = '{5} -> {3} -> {7} ->  Null'
+    assert actual == expected
+
+#Can successfully insert after a node in the linked list
+
+def test_insert_after():
+    second_obj = LinkedListInsertion()
+    
+    second_obj.append(1)
+    second_obj.append(2)
+    second_obj.append(3)
+    second_obj.append(5)
+    second_obj.add_after(3,6)
+
+    expected = str(second_obj)
+    actual = '{1} -> {2} -> {3} -> {6} -> {5} ->  Null'
+    assert actual == expected
+    
+# if the value was not exist will return same 
+def test_insert_after():
+    second_obj = LinkedListInsertion()
+    
+    second_obj.append(1)
+    second_obj.append(2)
+    second_obj.append(3)
+    second_obj.append(5)
+    second_obj.add_after(7,6)
+
+    expected = str(second_obj)
+    actual = '{1} -> {2} -> {3} -> {5} ->  Null'
+    assert actual == expected
+
+#Can successfully insert a node before the node of a linked list
+def test_insert_before():
+    third_obj = LinkedListInsertion()
+    third_obj.append(1)
+    third_obj.append(2)
+   
+    third_obj.add_before(2,6)
+
+    expected = str(third_obj)
+    actual = '{1} -> {6} -> {2} ->  Null'
+
     assert actual == expected
