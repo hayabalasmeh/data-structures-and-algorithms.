a245fbe74e1ecc54fb88e604f03246a97711c617
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-18 00:46:05 +0300

    adding readme

--- tests/test_fuzz_buzz.py
+++ tests/test_fuzz_buzz.py
@@ -29,4 +29,17 @@ def test_kary_tree():
     #Assert
     assert tree.root.value == 'Fizz' and tree.root.children_pointers[0].value == "Buzz" and tree.root.children_pointers[1].value == 'FizzBuzz'
 
+# Check if it return the string value of the number in case the value was numbers other than 3,5,15
+
+def test_other_numbers():
+    #Arrange
+    tree = KaryTree()
+    tree.root = Node(3)
+    tree.root.children_pointers.append(Node(7))
+    tree.root.children_pointers.append(Node(15))
     
+    #Act
+    actual = tree_fizz_buzz(tree.root)
+    
+    #Assert
+    assert tree.root.children_pointers[0].value == "7" 
