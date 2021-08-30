5b96292d4a650d0ba3d515cafcbfe734ab6f1a33
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-30 23:24:47 +0300

    adding merge-sort

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # data-structures-and-algorithms
-## Table of content of Code Chellenges (401);
 
- | Fourth Level; Code (401) | Links for the challenges | 
+## Table of content of Code Chellenges (401)
+
+ | Fourth Level; Code (401) | Links for the challenges |
  | ----------- | -------------------------------- |
  | First challenge |    [First one](challenge_one)  |
  | Second challenge |    [Second one](challenge_2)  |
---
+++ data_structures_algorth/challenge_merge_sort/merge_sort.py
@@ -0,0 +1,42 @@
+
+from typing import List
+
+
+def mergesort(arr):
+    n = len(arr)
+
+    if n > 1:
+      mid = n//2
+      left = arr[:mid]
+      right = arr[mid:n]
+      
+      mergesort(left)
+      
+      mergesort(right)
+      
+      merge(left, right, arr)
+    return arr
+
+def merge(left, right, arr):
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i = i + 1
+        else:
+            arr[k] = right[j]
+            j = j + 1
+
+        k = k + 1
+
+    if i == len(left):
+        arr[j-len(right)]= right[i-len(right)]
+    else:
+       arr[j-len(left)]= left[i-len(left)]
+
+if __name__=="__main__":
+    arr = [8,4,23,42,16,15]
+    print(mergesort(arr))
